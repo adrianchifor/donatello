9195b5eb7067514d6a72ef8a481d2e8c7747ba7f
uberspot <onexemailx@gmail.com>
2018-12-01 12:35:21 +0000

    Add makefile, minor code cleanup, setup response.

---
+++ donatello/utils.py
@@ -0,0 +1,8 @@
+import socket
+
+def getFunctionPublicIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return str(ip)
