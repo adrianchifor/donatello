071257adc1f3dbafd840099cea21e091b5f7d343
Adrian Chifor <me@adrianchifor.com>
2018-12-01 18:08:54 +0000

    Initial logic for bot

--- donatello/main.py
+++ donatello/main.py
@@ -3,7 +3,7 @@ import os
 import sys
 import ccxt
 
-import githubapi, payment, utils
+import githubapi, payment, utils, tip
 
 from flask import jsonify
 
@@ -17,7 +17,14 @@ if not (BINANCE_API_KEY and BINANCE_SECRET_KEY and GITHUB_TOKEN and GITHUB_WEBHO
     raise Exception("Make sure you've set BINANCE_API_KEY, BINANCE_SECRET_KEY, GITHUB_TOKEN +"
                     "and GITHUB_WEBHOOK_SECRET as environment variables")
 
-exchange = None
+exchange = ccxt.binance({
+    "apiKey": BINANCE_API_KEY,
+    "secret": BINANCE_SECRET_KEY,
+    "enableRateLimit": True,
+    "options": {
+        "adjustForTimeDifference": BINANCE_ADJUST_TIME
+    },
+})
 
 
 def main(request):
@@ -25,8 +32,9 @@ def main(request):
     Responds to any HTTP request.
     :param request: flask.Request
     """
+    tickers, balance = None
     try:
-        init_exchange()
+        tickers, balance = init_exchange()
     except Exception as e:
         print(e)
 
@@ -34,7 +42,60 @@ def main(request):
     try:
         request_json = request.get_json()
         gh = githubapi.GithubAPI(token=GITHUB_TOKEN, webhook_secret=GITHUB_WEBHOOK_SECRET)
-        gh.webhook(request=request_json)
+        event = gh.webhook(request=request_json)
+        if event:
+            if event["body"].startswith("/tip"):
+                if exchange and tickers and balance:
+                    amount_tipped = tip.parse_tip(event["body"])
+                    if amount_tipped:
+                        if amount_tipped < 5.0:
+                            gh.comment(event["repo_name"], event["pr_name"], "Minimum tip is $5")
+                        else:
+                            coins_supported = []
+                            for coin, amount in balance.items():
+                                coin_total_in_usd = payment.coin_total_usd(coin, amount, tickers)
+                                if coin_total_in_usd > amount_tipped:
+                                    coins_supported.append(coin)
+
+                            if len(coins_supported) > 0:
+                                comment = f"Coins available for tipping: {coins_supported}. " +
+                                          "Please redeem by commenting in the following format:\n" +
+                                          "```\n/redeem <COIN> <ADDRESS>\n```"
+                                gh.comment(event["repo_name"], event["pr_name"], comment)
+                            else:
+                                gh.comment(event["repo_name"], event["pr_name"], "Sorry, not enough funds.")
+                    else:
+                        gh.comment(event["repo_name"], event["pr_name"], "Failed to parse tip message")
+                else:
+                    print("exchange, tickers or balance not initialised")
+
+
+            elif event["body"].startswith("/redeem"):
+                comments = gh.get_comments(event["repo_name"], event["pr_name"])
+                comments_with_tip = []
+                comments_with_redeem = []
+                for comment in comments:
+                    if comment.startswith("/tip"):
+                        comments_with_tip.append(comment)
+                    elif comment.startswith("/redeem"):
+                        comments_with_redeem.append(comment)
+
+                if len(comments_with_tip) > 0 and len(comments_with_redeem) > 0:
+                    coin, address = tip.parse_redeem(comments_with_redeem[-1])
+                    amount = tip.parse_tip(comments_with_tip[-1])
+
+                    if coin and address:
+                        if amount:
+                            if exchange and tickers:
+                                crypto_amount = payment.coin_amount_for_usd(coin, amount, tickers)
+                                payment.withdraw(exchange, coin, crypto_amount, address)
+                            else:
+                                print("exchange or tickers not initialised")
+                        else:
+                            gh.comment(event["repo_name"], event["pr_name"], "Failed to parse tip message")
+                    else:
+                        gh.comment(event["repo_name"], event["pr_name"], "Failed to parse redeem message")
+
         # Build response
         response['functionPublicIP'] = utils.getFunctionPublicIP() # optional/debugging
         response['inputRequest'] = request_json
@@ -46,15 +107,6 @@ def main(request):
 
 
 def init_exchange():
-    exchange = ccxt.binance({
-        "apiKey": BINANCE_API_KEY,
-        "secret": BINANCE_SECRET_KEY,
-        "enableRateLimit": True,
-        "options": {
-            "adjustForTimeDifference": BINANCE_ADJUST_TIME
-        },
-    })
-
     tickers = exchange.fetch_tickers()
     balance = payment.filter_balance(exchange.fetch_total_balance(), tickers)
     print(f"Balance: {balance}")
@@ -65,6 +117,8 @@ def init_exchange():
 
     test_amount = payment.coin_amount_for_usd("XLM", 5.0, tickers)
     print(f"Amount for $5: {test_amount} XLM")
+
+    return tickers, balance
 
 
 if __name__ == '__main__':
